b678d29f867eba251697e18d93bc8f1285077d50
Emmanuel SARPEDON <sarped_e@etna-alternance.net>
2022-01-03 12:47:05 +0400

    update docker image tag

--- DockerApi/scripts_devOps/kubernetes-deployment.sh
+++ DockerApi/scripts_devOps/kubernetes-deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 minikube start
-kubectl create deployment vinted --image=emmanuelsarpedon/vinted:init
+kubectl create deployment vinted --image=emmanuelsarpedon/vinted:latest
 kubectl expose deployment vinted --type=NodePort --port=3000
 minikube service vinted --url
