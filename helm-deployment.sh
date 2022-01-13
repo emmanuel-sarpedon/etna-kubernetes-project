be38a15f0a3695e7d6b50c2d5df8e15d9b696b79
Emmanuel SARPEDON <sarped_e@etna-alternance.net>
2022-01-13 10:40:09 +0400

    update kubernetes deployment and scaling scripts

--- helm-deployment.sh
+++ helm-deployment.sh
@@ -1,5 +1,1 @@
-helm install vinted-chart ./helm/vinted/ --values ./helm/vinted/values.yaml
-
-export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services vinted-chart)
-export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
-echo http://$NODE_IP:$NODE_PORT
+helm install vinted-chart ./helm/vinted/ --values ./helm/vinted/values.yaml && export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services vinted-chart) && export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}") && echo http://$NODE_IP:$NODE_PORT
