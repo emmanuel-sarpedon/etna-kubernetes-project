64bd027f48efc27cae73269e1c7df504a9727e5b
Emmanuel SARPEDON <sarped_e@etna-alternance.net>
2022-01-13 10:46:52 +0400

    fix kubernetes script

--- kubernetes_deploy_and_expose.sh
+++ kubernetes_deploy_and_expose.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 
-kubectl run vinted-pod --image='local/vinted' --image-pull-policy='Never' --expose=true --port=3000
-kubectl port-forward vinted-pod 4242:3000
+kubectl apply -f ./kubernetes/
